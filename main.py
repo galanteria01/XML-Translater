c00efe172c09894266d0e69e791a1222359f10aa
shanuu <shanuu12e@gmail.com>
2020-11-06 11:13:34 +0530

    Something left to figure

--- main.py
+++ main.py
@@ -17,22 +17,45 @@ rootTree = tree.getroot()
 
 choicesToDo = ["Android studio Translations", "Text-translation"]
 for i in range(0,2):
-    print(str(i+1)+".",choicesToDo[i])
+    print(str(i+1)+".", choicesToDo[i])
 choiceMade = int(input("Enter your choice:- "))
+if choiceMade == 1:
 
-for i in range(0,len(nameOfLanguages)):
-    print(i+1,nameOfLanguages[i].name)
-previousLanguageIndex = int(input("Enter the previous language of XML file "))
-previousLanguage = nameOfLanguages[previousLanguageIndex]
+    for i in range(0,len(nameOfLanguages)):
+        print(i+1,nameOfLanguages[i].name)
+    previousLanguageIndex = int(input("Enter the previous language of XML file "))
+    previousLanguage = nameOfLanguages[previousLanguageIndex]
 
-for i in range(0, len(nameOfLanguages)):
-    print(i+1, nameOfLanguages[i].name)
-nextLanguageIndex = int(input("Enter the language to get of XML file "))
-nextLanguage = nameOfLanguages[nextLanguageIndex]
+    for i in range(0, len(nameOfLanguages)):
+        print(i+1, nameOfLanguages[i].name)
+    nextLanguageIndex = int(input("Enter the language to get of XML file "))
+    nextLanguage = nameOfLanguages[nextLanguageIndex]
+
+    for string in rootTree.findall('string'):
+        translatedText = translater.translate(str(string.text)
+                                              , src=previousLanguage.shortForm, dest=nextLanguage.shortForm)
+        string.text = str(translatedText.text)
+
+    tree.write('output.xml')
+
+elif choiceMade == 2:
+    for i in range(0,len(nameOfLanguages)):
+        print(i+1,nameOfLanguages[i].name)
+    previousLanguageIndex = int(input("Enter the previous language of XML file "))
+    previousLanguage = nameOfLanguages[previousLanguageIndex]
+
+    for i in range(0, len(nameOfLanguages)):
+        print(i+1, nameOfLanguages[i].name)
+    nextLanguageIndex = int(input("Enter the language to get of XML file "))
+    nextLanguage = nameOfLanguages[nextLanguageIndex]
+
+    textToTranslate = input("Enter the word to translate ")
+    srce = previousLanguage.shortForm
+    deste = nextLanguage.shortForm
+    translatedText = translater.translate(textToTranslate, src=srce, dest=deste)
+    print(translatedText.text)
 
 
-for string in rootTree.findall('string'):
-    translatedText = translater.translate(str(string.text), src=previousLanguage.shortForm, dest=nextLanguage.shortForm)
-    string.text = "Hello world"
-
+else:
+    quit()
 
